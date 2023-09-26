06143c7e1dec0a3476ee650773d56159d3d97666
Clement Ponssard <145286934+ClementPonssard@users.noreply.github.com>
2023-09-26 10:39:35 +0200

    Update auclairdelalune.ino

--- auclairdelalune.ino
+++ auclairdelalune.ino
@@ -1,19 +1,25 @@
-int duree=500;
+int noire=500; // durée d'une noire en ms
+int pinBuz = 8 ; // broche sur laquelle est connectée le buzzer
 
-int pinBuz = 8 ;
+// les fréquences des notes de l'octave 3
+int do3 = 262 ; // en Hz
+int re3 = 294 ;
+int red3 = 311 ;
+int mi3 = 330 ;
 
 void  setup() {
-  tone(pinBuz,262,duree);delay(duree*1.1);
-  tone(pinBuz,262,duree);delay(duree*1.1);
-  tone(pinBuz,262,duree);delay(duree*1.1);
-  tone(pinBuz,294,duree);delay(duree*1.1);
-  tone(pinBuz,330,2*duree);delay(2*duree*1.1);
-  tone(pinBuz,294,2*duree);delay(2*duree*1.1);
-  tone(pinBuz,262,duree);delay(duree*1.1);
-  tone(pinBuz,330,duree);delay(duree*1.1);
-  tone(pinBuz,294,duree);delay(duree*1.1);
-  tone(pinBuz,294,duree);delay(duree*1.1);
-  tone(pinBuz,262,4*duree);delay(4*duree*1.1);
+  tone(pinBuz,do3,noire);delay(noire*1.1);
+  tone(pinBuz,do3,noire);delay(noire*1.1);
+  tone(pinBuz,do3,noire);delay(noire*1.1);
+  tone(pinBuz,re3,noire);delay(noire*1.1);
+  tone(pinBuz,mi3,2*noire);delay(2*noire*1.1);
+  tone(pinBuz,re3,2*noire);delay(2*noire*1.1);
+  tone(pinBuz,do3,noire);delay(noire*1.1);
+  tone(pinBuz,mi3,noire);delay(noire*1.1);
+  tone(pinBuz,re3,noire);delay(noire*1.1);
+  tone(pinBuz,re3,noire);delay(noire*1.1);
+  tone(pinBuz,do3,4*noire);delay(4*noire*1.1);
 }
+
 void  loop() {
 }
